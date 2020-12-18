9fe75922b163e6fbb3f807ebac306147caf911ae
xudongyi <1922448115@qq.com>
2020-12-18 11:30:44 +0800

    短信发送次数提醒 手机端路由代码不刷新

--- src/main/java/business/service/impl/OathServiceImpl.java
+++ src/main/java/business/service/impl/OathServiceImpl.java
@@ -240,7 +240,7 @@ public class OathServiceImpl implements IOauthService {
                 operateLog.setContent("发送短信,验证码为："+code);
                 log.debug("loginLog===="+ operateLog);
                 iOperateLogService.save(operateLog);
-                return Result.ok("发送成功,本月已发送："+list.size()+"次");
+                return Result.ok("发送成功,本月已发送："+(list.size()+1)+"次");
             }else{
                 return Result.error(result.get("description"));
             }
